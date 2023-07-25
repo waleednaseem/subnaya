cd871d019fa221e9791327630f7a52165426bbf0
aliya@gmail.com <aliya@gmail.com>
2023-07-25 22:54:05 +0500

    aliya

--- src/Components/Brand/BannerLable/index.js
+++ src/Components/Brand/BannerLable/index.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function index({BannerLable}) {
   return (
     <div>
-  <h2 className=' text-white font-bold text-2xl justify-center flex'> {BannerLable}</h2>
+  <h2 className=' text-white font-bold text-2xl justify-center flex pt-3'> {BannerLable}</h2>
 
 
     </div>
--- src/Components/Brand/RightSection/ProductBanner.js
+++ src/Components/Brand/RightSection/ProductBanner.js
@@ -2,23 +2,18 @@ import React from 'react'
 import BannerLable from '../BannerLable'
 
 export default function ProductBanner() {
-  const array=[
-    {
-
-      heading:"" 
-    }
-    
+ 
   
   
-  ]
+  
   return (
     <div>
- <div className='grid grid-cols-1 bg-blue-gray-600 h-20 m-2 '>
+ <div className='grid grid-cols-1 bg-blue-gray-600 h-20  mt-2 mb-2 '>
 
 <BannerLable  BannerLable={"HUMDUM"}/>
         
         {/* <h1 className=' text-white font-bold text-2xl justify-center flex'> HUMDUM</h1> */}
-        <button className=' bg-pink-500 w-52 rounded-xl mx-auto '>
+        <button className=' w-52 rounded-md mx-auto  bg-black text-white mb-2'>
            LAWN COLLECTION 2023
         </button>
            
